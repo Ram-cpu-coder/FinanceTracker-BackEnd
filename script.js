a27b7ecff075e7feb2b0de27cf69b61ffdef616c
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-01-21 12:28:12 +1100

    transaction create and delete

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import mongoose from "mongoose";
 import { connectMongoDB } from "./config/mongodbConfig.js";
 import bcrypt from "bcrypt"
+import jwt, { decode } from "jsonwebtoken"
 
 //connection to the mongo database
 connectMongoDB()
@@ -34,7 +35,36 @@ const userSchema = mongoose.Schema({
 //mongoose model
 const User = mongoose.model("user", userSchema)
 
+// transaction schema 
+const transactionSchema = mongoose.Schema({
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ["Income", "Expense"],
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: ""
+    }
 
+},
+    { timestamps: true })
+
+// model for transaction
+const Transaction = mongoose.model("Transaction", transactionSchema)
 
 app.get("/", (req, res) => {
     res.json({
@@ -88,11 +118,17 @@ app.post("/api/v1/users/login", async (req, res) => {
         const userData = await User.findOne({ email });
         if (userData) {
             const loginSuccess = await bcrypt.compare(password, userData.password)
+            const tokenData = {
+                email: userData.email,
+            }
+            const token = await jwt.sign(tokenData, process.env.JWT_SECRET, {
+                expiresIn: process.env.JWT_EXPIRES_IN,
+            })
             if (loginSuccess) {
                 res.status(200).json({
                     status: "success",
                     message: "Login Successful",
-                    userData
+                    accessToken: token,
                 })
             }
             else {
@@ -116,9 +152,149 @@ app.post("/api/v1/users/login", async (req, res) => {
 
 
 })
+// create the transaction
+app.post("/api/v1/transactions/add", async (req, res) => {
+    try {
+        //1. get the token
+        const token = req.headers.authorization;
 
+        // 2. verify the token 
+        const decodedData = await jwt.verify(token, process.env.JWT_SECRET);
 
+        if (decodedData?.email) {
 
+            // 3. find the user from the decoded data
+            const userData = await User.findOne({ email: decodedData.email })
+
+            if (userData) {
+                // 4. create the transaction
+                const { type, description, amount, date } = req.body
+                const newTransaction = new Transaction({
+                    userID: userData._id,
+                    type,
+                    description,
+                    amount,
+                    date
+                })
+                const newData = await newTransaction.save();
+                res.status(201).json({
+                    status: "success",
+                    message: "Transaction Created",
+                    Transaction: newData
+                })
+            } else {
+                res.status(404).json({
+                    status: "error",
+                    message: "User Not found",
+                })
+            }
+
+        } else {
+            res.status(401).json({
+                status: "error",
+                message: "No payload",
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            status: "error",
+            message: error.message,
+        })
+    }
+})
+// get the transaction
+app.get("/api/v1/transactions", async (req, res) => {
+    try {
+        // 1. get the token
+        const token = req.headers.authorization
+
+        // 2. verify the token 
+        const decodedData = await jwt.verify(token, process.env.JWT_SECRET)
+
+        if (decodedData?.email) {
+            // 3. find the user from the decoded data 
+            const userData = await User.findOne({ email: decodedData.email })
+            if (userData) {
+                // 4. fetch the transaction data of the user with the user id from the userData
+                const transactionData = await Transaction.find({
+                    userID: userData._id
+                })
+                res.status(201).json({
+                    status: "success",
+                    message: "Transaction found",
+                    transactionData
+                })
+            }
+            else {
+                res.status(401).json({
+                    status: "error",
+                    message: "Transaction Not found",
+                })
+            }
+
+        } else {
+            res.status(401).json({
+                status: "error",
+                message: "No payload",
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            status: "error",
+            message: "Error fetching the transaction",
+        })
+    }
+})
+//delete the transaction 
+app.delete("/api/v1/transactions/delete/:id"), async (req, res) => {
+    try {
+        const id = req.params.id
+        // 1. get the token 
+        const token = req.headers.authorization
+        console.log(token, 300)
+        // 2. verify the token 
+        const decodedData = await jwt.verify(token, process.env.JWT_SECRET)
+
+        // 3. find the userData
+        const userData = User.findOne({ email: decodedData.email })
+        console.log(userData)
+        if (decodedData) {
+            if (userData) {
+                // 4. find the transaction 
+                const transactionData = Transaction.findById(id)
+                console.log(transactionData)
+
+                res.status(201).json({
+                    status: "success",
+                    message: "Transaction Deleted",
+                    filteredData
+                })
+            }
+            else {
+                res.status(401).json({
+                    status: "error",
+                    message: "Not Authorised",
+                })
+            }
+        } else {
+            res.status(401).json({
+                status: "error",
+                message: "Could not decode",
+            })
+        }
+        res.status(200).json({
+            message: "error"
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: "Internal Error"
+        })
+    }
+}
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log('Server started')
 })
